efb8ef502b6e0fc4702b810320cb081c7a1a4b17
Alishow1111 <alishow1111@gmail.com>
2023-11-20 14:07:12 +0000

    Completed ex3

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,21 @@
-const {fetchTopics} = require("../models/model.js");
+const {fetchTopics, fetchEndpoints} = require("../models/model.js");
 
 exports.getTopics = (req,res,next) => {
     fetchTopics().then((topics) => {
         res.status(200).send({topics});
     })
     .catch((err) => {
+        next(err);
+    })
+}
+
+exports.getEndpoints = (req,res,next) => {
+    fetchEndpoints().then((endpoints) => {
+        console.log(endpoints);
+        res.status(200).send(endpoints);
+    })
+    .catch((err) => {
+        console.log(err);
         next(err);
     })
 }
--- models/model.js
+++ models/model.js
@@ -1,7 +1,18 @@
 const db = require("../db/connection.js");
+const fs = require('fs/promises')
 
 exports.fetchTopics = () => {
     return db.query("SELECT * FROM topics").then((result) => {
         return result.rows;
+    })
+}
+
+exports.fetchEndpoints = () => {
+    return fs.readFile(`${__dirname}/../endpoints.json`).then((result) => {
+        const endpointObject = JSON.parse(result);
+        return endpointObject;
+    })
+    .catch((err) => {
+        console.log("error");
     })
 }
